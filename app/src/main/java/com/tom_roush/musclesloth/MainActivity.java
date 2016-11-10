b38b82b64feca202964d9a2625cbfbf5aafac07d
temandr <brentwoodpav@gmail.com>
2016-11-10 12:05:59 -0600

    added onClicks

--- app/src/main/java/com/tom_roush/musclesloth/MainActivity.java
+++ app/src/main/java/com/tom_roush/musclesloth/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,11 +14,21 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// Resources for the given activity
 		Resources res = getResources();
 
+		// Update image
 		ImageView im = (ImageView) findViewById(R.id.front_image);
-
 		im.setImageResource(R.drawable.sloth_logo);
+	}
 
+	public void startWorkActivity(View v)
+	{
+		Toast.makeText(this, "Workout", Toast.LENGTH_LONG).show();
+	}
+
+	public void startArcActivity(View v)
+	{
+		Toast.makeText(this, "Arc", Toast.LENGTH_LONG).show();
 	}
 }
