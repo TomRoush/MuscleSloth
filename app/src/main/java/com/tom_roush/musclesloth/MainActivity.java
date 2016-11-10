fe02145f508c376b13bf64fbcac170974cd0bc74
temandr <brentwoodpav@gmail.com>
2016-11-10 11:48:17 -0600

    added image

--- app/src/main/java/com/tom_roush/musclesloth/MainActivity.java
+++ app/src/main/java/com/tom_roush/musclesloth/MainActivity.java
@@ -1,13 +1,22 @@
 package com.tom_roush.musclesloth;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Resources res = getResources();
+
+		ImageView im = (ImageView) findViewById(R.id.front_image);
+
+		im.setImageResource(R.drawable.sloth_logo);
+
 	}
 }
