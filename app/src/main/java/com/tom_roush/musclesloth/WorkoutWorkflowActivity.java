8d3f815c626b393615ad393886d990bff06b91d4
temandr <brentwoodpav@gmail.com>
2016-11-12 16:07:42 -0600

    added the workflow activity

---
+++ app/src/main/java/com/tom_roush/musclesloth/WorkoutWorkflowActivity.java
@@ -0,0 +1,14 @@
+package com.tom_roush.musclesloth;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutWorkflowActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_workflow);
+    }
+}
