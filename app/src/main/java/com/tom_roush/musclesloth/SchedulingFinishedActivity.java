fa8484edb18879dc20624a497ed0eb8303c8caec
Tom Roush <birdbrain2@comcast.net>
2016-11-17 09:14:35 -0600

    Improve scheduling

--- app/src/main/java/com/tom_roush/musclesloth/SchedulingFinishedActivity.java
+++ app/src/main/java/com/tom_roush/musclesloth/SchedulingFinishedActivity.java
@@ -1,7 +1,9 @@
 package com.tom_roush.musclesloth;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SchedulingFinishedActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SchedulingFinishedActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scheduling_finished);
+	}
+
+	public void returnToFront(View v) {
+		startActivity(new Intent(this, MainActivity.class));
 	}
 }
