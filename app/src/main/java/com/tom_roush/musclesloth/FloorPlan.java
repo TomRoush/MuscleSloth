6bc5a02f7baa8a57282581aff0e7e5e29b605463
Tom Roush <birdbrain2@comcast.net>
2016-11-17 17:18:51 -0600

    Fix

--- app/src/main/java/com/tom_roush/musclesloth/FloorPlan.java
+++ app/src/main/java/com/tom_roush/musclesloth/FloorPlan.java
@@ -2,30 +2,24 @@ package com.tom_roush.musclesloth;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.View.OnClickListener;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Button;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -205,8 +199,6 @@ public class FloorPlan extends AppCompatActivity {
                 currentPage = 1;
                 imageView.setImageResource(R.drawable.arc_court);
             }
-
-                if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
 
             if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
                 currentPage = 2;
