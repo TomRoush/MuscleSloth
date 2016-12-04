7ccc574a386336ec672d553873976ad7e58932e8
nathan-whitney <nwhitne2@illinois.edu>
2016-12-03 21:01:59 -0600

    Changed search page to include all machines by default, will create further search function soon(tm)

--- app/src/main/java/com/tom_roush/musclesloth/SearchActivity.java
+++ app/src/main/java/com/tom_roush/musclesloth/SearchActivity.java
@@ -23,14 +23,29 @@ public class SearchActivity extends AppCompatActivity {
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        mainListView = (ListView) findViewById(R.id.freeListView);
 
-        String[] machines = new String[]{"Flat Bench 1", "Flat Bench 2", "Incline Bench 1", "Incline Bench 2", "Decline Bench 1", "Decline Bench 2"};
+        String[] free = new String[]{"Flat Bench 1", "Flat Bench 2", "Incline Bench 1", "Incline Bench 2", "Decline Bench 1", "Decline Bench 2"};
+        ArrayList<String> freeList = new ArrayList<>();
+        freeList.addAll(Arrays.asList(free));
+
+        String[] machines = new String[]{"Hip Abductor", "Assisted Pull-ups", "Cable Tower", "Leg Curl", "Leg Press", "Chest Flies"};
         ArrayList<String> machinesList = new ArrayList<>();
         machinesList.addAll(Arrays.asList(machines));
 
-        listAdapter = new ArrayAdapter<String>(SearchActivity.this, R.layout.simplerow, machinesList);
+        String[] cardio = new String[]{"Treadmill", "Elliptical", "Stair Stepper", "Stationary Bike", "Incline Treadmill", "Free-Move Elliptical"};
+        ArrayList<String> cardioList = new ArrayList<>();
+        cardioList.addAll(Arrays.asList(cardio));
 
+        listAdapter = new ArrayAdapter<String>(SearchActivity.this, R.layout.simplerow, freeList);
+        mainListView = (ListView) findViewById(R.id.freeListView);
+        mainListView.setAdapter(listAdapter);
+
+        listAdapter = new ArrayAdapter<String>(SearchActivity.this, R.layout.simplerow, machinesList);
+        mainListView = (ListView) findViewById(R.id.machineListView);
+        mainListView.setAdapter(listAdapter);
+
+        listAdapter = new ArrayAdapter<String>(SearchActivity.this, R.layout.simplerow, cardioList);
+        mainListView = (ListView) findViewById(R.id.cardioListView);
         mainListView.setAdapter(listAdapter);
     }
 }
